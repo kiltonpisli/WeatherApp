ba069488db2cc2102f32997dec14f769dbccb0a7
Kilton Pisli <kiltonpisli@gmail.com>
2022-03-16 23:41:00 +0100

    found layout and bg images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import CurrentWeather from './components/CurrentWeather.js'
-import { useGetYourCurrentWeatherQuery } from './features/weatherApi'
+// import { useGetYourCurrentWeatherQuery } from './features/weatherApi'
 
-let myPosition = { lat:"", lon: ""}
-navigator.geolocation.getCurrentPosition(function(position) {
-    myPosition.lat = position.coords.latitude;
-    myPosition.lon = position.coords.longitude;
-});
+// let myPosition = { lat:"", lon: ""}
+// navigator.geolocation.getCurrentPosition(function(position) {
+//     myPosition.lat = position.coords.latitude;
+//     myPosition.lon = position.coords.longitude;
+// });
 
 function App() {
-  const {data: currentWeatherData, isLoading, isSuccess, isError} = useGetYourCurrentWeatherQuery(myPosition);
-  console.log(currentWeatherData);
+  // const {data: currentWeatherData, isLoading, isSuccess, isError} = useGetYourCurrentWeatherQuery(myPosition);
+  // console.log(currentWeatherData);
 
   return (
     <div className="App">
