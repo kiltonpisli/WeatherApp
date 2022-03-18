e6d5bfb8a49ce9ec7cd448468ff533c480d8b9a1
Kilton Pisli <kiltonpisli@gmail.com>
2022-03-18 02:41:43 +0100

    crated .env file for api_key

--- frontend/src/features/weatherApi.js
+++ frontend/src/features/weatherApi.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_KEY = "61d78934f9af1172aafa64be8e1e8bf8";
 const BASE_URL = "https://pro.openweathermap.org/data/2.5";
 
 export const weatherApi = createApi({
@@ -8,16 +7,16 @@ export const weatherApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     getYourCurrentWeather: builder.query({
-      query: ({lat, lon}) => `/weather?appid=${API_KEY}&lat=${lat}&lon=${lon}&units=metric`,
+      query: ({lat, lon}) => `/weather?appid=${process.env.REACT_APP_WEATHER_API_KEY}&lat=${lat}&lon=${lon}&units=metric`,
     }),
     getSearchCurrentWeather: builder.query({
-      query: (city) => `/weather?appid=${API_KEY}&q=${city}&units=metric`,
+      query: (city) => `/weather?appid=&q=${city}&units=metric`,
     }),
     getYourNext7DaysWeather: builder.query({
-      query: ({lat, lon}) => `/daily?appid=${API_KEY}&lat=${lat}&lon=${lon}&units=metric&cnt=8`,
+      query: ({lat, lon}) => `/daily?appid=&lat=${lat}&lon=${lon}&units=metric&cnt=8`,
     }),
     getSearchNext7DaysWeather: builder.query({
-      query: (city) => `/daily?appid=${API_KEY}&q=${city}&units=metric&cnt=8`,
+      query: (city) => `/daily?appid=&q=${city}&units=metric&cnt=8`,
     }),
   }),
 })
